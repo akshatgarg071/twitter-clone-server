a15b2854dc4ed8ca681f74b4a0c85e0f65c92a15
sdshuvo98 <hellosdshuvo@gamil.com>
2022-08-16 12:29:31 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://twitter_admin:XkV80JAqGQBEEkrB@cluster0.bpb775a.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const postCollection = client.db("database").collection("posts");
+
+        // start from here
+
+
+    } catch (error) {
+        console.log(error);
+    }
+} run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello from Twitter Clone!')
