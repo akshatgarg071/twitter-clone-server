7542b2f8cf176b166b4171c6b5b4391ff800bcfb
sravani <narrasravni02@gmail.com>
2022-08-25 21:59:39 +0530

    commited

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://twitter_admin:XkV80JAqGQBEEkrB@cluster0.bpb775a.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const postCollection = client.db("database").collection("posts"); // this collection is for team-ekt
+        const userCollection = client.db("database").collection("users"); // this collection is for team-srv
+
+        // get
+        app.get('/user', async (req, res) => {
+            const user = await userCollection.find().toArray();
+            res.send(user);
+        })
+        app.get('/loggedInUser', async (req, res) => {
+            const email = req.query.email;
+            const user = await userCollection.find({ email: email }).toArray();
+            res.send(user);
+        })
+
+        app.get('/post', async (req, res) => {
+            const post = (await postCollection.find().toArray()).reverse();
+            res.send(post);
+        })
+
+        // post
+        app.post('/register', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.post('/post', async (req, res) => {
+            const post = req.body;
+            const result = await postCollection.insertOne(post);
+            res.send(result);
+        })
+
+
+    } catch (error) {
+        console.log(error);
+    }
+} run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Twitter Clone!')
+})
+
+app.listen(port, () => {
+    console.log(`Twitter clone is listening on port ${port}`)
+})
